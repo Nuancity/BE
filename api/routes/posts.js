9f5283ce8b566b08ffd7f31232a245695ca204e8
Yanna Faith <kfaithc97@gmail.com>
2019-12-03 22:06:57 -0700

    Basic Crud for posts

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authHelper = require('../../common/auth_helper.js');
 const posts = require('../../database/models/posts_model.js');
+const db = require('../../database/db_config.js');
 
 router.get('/', ( req, res ) => {
    posts.getAll()
@@ -13,47 +14,38 @@ router.get('/', ( req, res ) => {
    })
 });
 
-router.get('/:id', async (req, res) => {
-    const { id } = req.params || req.body
+router.get('/:id', async ( req, res ) => {
+    const { id } = req.params || req.body;
     try {
        const post = await db( 'posts' )
           .where({ id: id })
           .first();
- 
        !post
           ? res.status( 404 ).json({ error: 'post does not exist' })
-          : res.status( 200 ).json({ post, post_categories });
+          : res.status( 200 ).json({ post });
     } catch ( err ) {
-       res.status( 500 ).json({ error: 'unable to get post' });
+       res.status( 500 ).json({ error: 'unable to get post', err });
     }
  });
 
 router.post('/', async ( req, res ) => {
-    let { id, ...newPost } = req.body
-    console.log( req.body );
-
+    let { id, ...newPost } = req.body;
     try {
-      const allPosts = await posts.insert( newPost );
-      newPost = await db( 'posts' ).where({ title: newPost.title }).first();
-  
-        res.status(201).json({
-           message: "post created!",
-           newPost, allPosts
-         })
-
-    } catch (err) {
-        res.status(500).json(err)
+      await posts.insert( newPost );
+      const allPosts = posts.getAll();
+      res.status( 201 ).json( allPosts )
+    } catch ( err ) {
+        res.status( 500 ).json( err )
     }
 });
 
-router.put('/:id', authHelper.protected, async (req, res) => {
-    const { id } = req.params
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
     const changes = req.body;
     try {
        const post = await db('posts')
           .where({ id: id })
           .first();
- 
        !post
           ? res.status(404).json({ error: 'post does not exist' })
           : await db('posts')
@@ -71,26 +63,17 @@ router.put('/:id', authHelper.protected, async (req, res) => {
     }
  });
 
-router.delete('/:id', authHelper.protected, async (req, res) => {
+router.delete('/:id', async (req, res) => {
     const { id } = req.params;
     try {
-         const del_post_categories = await db('post-categories').where({post_id:id}).del()
-
        const post = await db('posts')
           .where({ id: id })
           .first();
- 
        !post
           ? res.status(404).json({ error: 'post does not exist' })
-          : await db('posts')
-               .where({ id: id })
-               .delete();
-
-            allPosts = await db('posts')
- 
+          : await db('posts').where({ id: id }).delete(); 
        res.status(202).json({
           message: 'post has been deleted.',
-          allPosts
        });
     } catch (err) {
        res.status(500).json({ error: 'Unable to delete post' });
